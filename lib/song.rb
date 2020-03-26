781e4d8e8745c6838f6026f426cf056bfb7b5665
Maika Kailani <queenmab051@gmail.com>
2020-03-26 19:46:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,5 +53,9 @@ class Song
     m = name.gsub('.mp3', '').split(/\-/)
     s = self.create_by_name(x[1].strip)
     s.artist_name = m[0].strip
+    s
+  end
+  
+  def self.create_from_filename(name)
     
 end
